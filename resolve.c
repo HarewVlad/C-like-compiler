1f49cbc0290e76bd46b1a7f0ac4cd13339fa04e4
Vlad Petrenko <HarewVlad@mail.ru>
2019-05-01 10:28:09 +0300

    trying to resolve if_stmt

--- resolve.c
+++ resolve.c
@@ -5,53 +5,36 @@ typedef enum SymKind
 	SYM_RESOLVED
 } SymKind;
 
+typedef enum SymType
+{
+	SYM_DECL,
+	SYM_STMT,
+} SymType;
+
 typedef struct Sym
 {
 	SymKind kind;
-	Decl *decl;
+	SymType type;
+	union
+	{
+		Decl *decl;
+		Stmt *stmt;
+	};
 } Sym;
 
 Sym *global_table;
 Sym **local_table;
 
-void fill_symbol_table(Stmt *stmt)
-{
-	Sym *new_local_table = NULL;
-	switch (stmt->kind)
-	{
-	case STMT_DECL:
-		buf_push(global_table, (Sym) { SYM_UNRESOLVED, stmt->decl });
-		break;
-	case STMT_BLOCK:
-		for (size_t i = 0; i < buf_len(stmt->block.stmts); i++)
-		{
-			switch (stmt->block.stmts[i]->kind)
-			{
-			case STMT_DECL:
-				buf_push(new_local_table, (Sym) { SYM_UNRESOLVED, stmt->block.stmts[i]->decl });
-				break;
-			case STMT_BLOCK:
-				fill_symbol_table(stmt->block.stmts[i]);
-				break;
-			}
-		}
-		buf_push(local_table, new_local_table);
-		break;
-	default:
-		assert(0);
-		break;
-	}
-}
-
-Decl *get_sym(const char *name)
+Decl *get_sym_decl(const char *name) // TODO: add name to struct
 {
 	Decl *d = NULL;
 	for (size_t i = 0; i < buf_len(local_table); i++)
 	{
 		for (size_t j = 0; j < buf_len(local_table[i]); j++)
 		{
+			Sym s = local_table[i][j];
 			d = local_table[i][j].decl;
-			if (strcmp(d->name, name) == 0) // TODO: str interning
+			if (s.type == SYM_DECL && strcmp(d->name, name) == 0) // TODO: str interning
 			{
 				return d;
 			}
@@ -60,88 +43,63 @@ Decl *get_sym(const char *name)
 	return d;
 }
 
+void fill_local_table(Stmt *s);
+
+void fill_sym(Sym **new_local_table, Stmt *stmt) // TODO: for now assume thats all inside block, redo later
+{
+	switch (stmt->kind)
+	{
+	case STMT_DECL:
+		buf_push(*new_local_table, (Sym) { SYM_UNRESOLVED, SYM_DECL, stmt->decl });
+		break;
+	case STMT_IF: // TODO: resolve cond.
+		fill_local_table(stmt->if_stmt.then_block);
+		if (stmt->if_stmt.else_ifs != NULL)
+		{
+			for (size_t i = 0; i < buf_len(stmt->if_stmt.else_ifs); i++)
+			{
+				fill_local_table(stmt->if_stmt.else_ifs[i].block);
+			}
+		}
+		if (stmt->if_stmt.else_block != NULL)
+		{
+			fill_local_table(stmt->if_stmt.else_block);
+		}
+		break;
+	case STMT_ASIGN:
+		buf_push(*new_local_table, (Sym) { SYM_UNRESOLVED, SYM_STMT, stmt });
+		break;
+	default:
+		assert(0);
+		break;
+	}
+}
+
+// |1| - 1, 2, 3 |2| - 1, 2, 3
+
+void fill_local_table(Stmt *s)
+{
+	Sym *new_local_table = NULL;
+	for (size_t i = 0; i < buf_len(s->block.stmts); i++)
+	{
+		fill_sym(&new_local_table, s->block.stmts[i]);
+	}
+	buf_push(local_table, new_local_table);
+}
+
+void fill_another(Stmt *s)
+{
+	Sym *new_local_table = NULL;
+	for (size_t i = 0; i < buf_len(s->block.stmts); i++)
+	{
+		fill_local_table(s->block.stmts[i]);
+	}
+
+}
+
 // FOR NOW, only resolve local table
 
-typedef enum EntityKind
-{
-	ENTITY_NONE, // TODO: ENTITY_NULL ???
-	ENTITY_INTEGRAL,
-	ENTITY_DECL,
-	ENTITY_BINARY,
-} EntityKind;
-
-typedef struct Entity Entity;
-
-struct Entity
-{
-	EntityKind kind;
-	Typespec type;
-	union
-	{
-		Decl *decl;
-		Expr *expr;
-		struct
-		{
-			TokenKind op;
-			Entity *left;
-			Entity *right;
-		};
-	};
-};
-
-Entity *new_entity(EntityKind kind)
-{
-	Entity *e = malloc(sizeof(Entity));
-	e->kind = kind;
-	return e;
-}
-
-Entity *new_entity_none()
-{
-	Entity *e = new_entity(ENTITY_NONE);
-	return e;
-}
-
-Entity *new_entity_integral(Expr *expr)
-{
-	Entity *e = new_entity(ENTITY_INTEGRAL);
-	e->expr = expr;
-	return e;
-}
-
-Entity *new_entity_decl(Decl *decl)
-{
-	Entity *e = new_entity(ENTITY_DECL);
-	e->decl = decl;
-	return e;
-}
-
-Entity *new_entity_binary(TokenKind op, Entity *left, Entity *right)
-{
-	Entity *e = new_entity(ENTITY_BINARY);
-	e->op = op;
-	e->left = left;
-	e->right = right;
-	return e;
-}
-
-typedef struct EntityComplete // TODO: mb fuck that shit?
-{
-	Decl *decl;
-	Entity *entity;
-} EntityComplete;
-
-EntityComplete **entities;
-
-EntityComplete *new_entity_complete(Decl *decl, Entity *entity)
-{
-	EntityComplete *e_complete = malloc(sizeof(EntityComplete));
-	e_complete->decl = decl;
-	e_complete->entity = entity;
-	return e_complete;
-}
-
-Sym *resolve_name(size_t index, const char *name)
+Sym *resolve_name_decl(size_t index, const char *name)
 {
 	for (size_t i = index; i < buf_len(local_table); i++)
 	{
@@ -161,8 +119,7 @@ Sym *resolve_name(size_t index, const char *name)
 	return NULL;
 }
 
-
-void resolve_decl(size_t index_table, Sym *s, Decl *d);
+void resolve_sym(size_t index_table, Sym *s);
 
 void resolve_expr(size_t index_table, Sym *s, Expr *e)
 {
@@ -173,10 +130,10 @@ void resolve_expr(size_t index_table, Sym *s, Expr *e)
 		break;
 	case EXPR_NAME:
 		s->kind = SYM_RESOLVING;
-		Sym *new_s = resolve_name(index_table, e->name);
+		Sym *new_s = resolve_name_decl(index_table, e->name);
 		if (new_s != NULL)
 		{
-			resolve_decl(index_table, new_s, new_s->decl);
+			resolve_sym(index_table, new_s);
 		}
 		else
 			fatal("unresolved sym '%s'", e->name);
@@ -191,12 +148,12 @@ void resolve_expr(size_t index_table, Sym *s, Expr *e)
 	}
 }
 
-void resolve_decl(size_t index_table, Sym *s, Decl *d)
+void resolve_decl(size_t index_table, Sym *s)
 {
-	switch (d->kind)
+	switch (s->decl->kind)
 	{
 	case DECL_VAR:
-		resolve_expr(index_table, s, d->var.expr);
+		resolve_expr(index_table, s, s->decl->var.expr);
 		break;
 	default:
 		assert(0);
@@ -204,16 +161,62 @@ void resolve_decl(size_t index_table, Sym *s, Decl *d)
 	}
 }
 
-void resolve_decls()
+void resolve_stmt(size_t index_table, Sym *s)
+{
+	switch (s->stmt->kind)
+	{
+	case STMT_ASIGN:
+		resolve_expr(index_table, s, s->stmt->asign.e);
+		break;
+	case STMT_IF:
+		break;
+	default:
+		assert(0);
+		break;
+	}
+}
+
+void resolve_sym(size_t index_table, Sym *s)
+{
+	switch (s->type)
+	{
+	case SYM_DECL:
+		resolve_decl(index_table, s);
+		break;
+	case SYM_STMT:
+		resolve_stmt(index_table, s);
+		break;
+	default:
+		assert(0);
+		break;
+	}
+}
+
+void resolve()
 {
 	for (size_t i = 0; i < buf_len(local_table); i++)
 	{
 		for (size_t j = 0; j < buf_len(local_table[i]); j++)
 		{
 			Sym s = local_table[i][j];
-			Decl *d = local_table[i][j].decl;
-			resolve_decl(i, &s, d);
+			resolve_sym(i, &s);
 		}
+	}
+}
+
+void dump_sym(Sym s)
+{
+	switch (s.type)
+	{
+	case SYM_DECL:
+		dump_decl(s.decl);
+		break;
+	case SYM_STMT:
+		dump_stmt(s.stmt);
+		break;
+	default:
+		assert(0);
+		break;
 	}
 }
 
@@ -234,7 +237,7 @@ void dump_local_table()
 		printf("(Local table -> ");
 		for (size_t j = 0; j < buf_len(local_table[i]); j++)
 		{
-			dump_decl(local_table[i][j].decl);
+			dump_sym(local_table[i][j]);
 		}
 		printf(")");
 	}
